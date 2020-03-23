ae9f277572933b87faf44a258ff34670e2240246
yaroslavoz <yaroslavozv@gmail.com>
2020-03-23 23:52:08 +0200

    Updates

---
+++ precache-manifest.22a100fac24af575c83a43f31b05aa42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0634c74e91bb6658c9c314d05bf61b24",
+    "url": "/reactwarriors/index.html"
+  },
+  {
+    "revision": "9453b9874b4e63ecc059",
+    "url": "/reactwarriors/static/css/main.2b3663fb.chunk.css"
+  },
+  {
+    "revision": "ffad8f5bac1ed17cf667",
+    "url": "/reactwarriors/static/js/2.e345f4bf.chunk.js"
+  },
+  {
+    "revision": "9453b9874b4e63ecc059",
+    "url": "/reactwarriors/static/js/main.42d53a36.chunk.js"
+  },
+  {
+    "revision": "8c7a3e2c5a8852c9f017",
+    "url": "/reactwarriors/static/js/runtime~main.58be9cf0.js"
+  },
+  {
+    "revision": "d9361c371dc331a90d02bb72652a2384",
+    "url": "/reactwarriors/static/media/wall.d9361c37.jpg"
+  }
+]);
