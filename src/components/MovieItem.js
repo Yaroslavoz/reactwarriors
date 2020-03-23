0b12fc3b4b1937afcdf875bc1c54fa91faee1b27
yaroslavoz <yaroslavozv@gmail.com>
2020-03-24 00:12:30 +0200

    text styling

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -37,7 +37,7 @@ class MovieItem extends React.Component {
                 
               <div className="font-bold text-xl mb-2 p-2">{movie.title}</div>
               
-                {this.state.show === true ? <p className='z-40 p-2 text-gray-700 text-base'>{movie.overview}</p> : null}
+                {this.state.show === true ? <p className='z-40 p-2 text-white text-base'>{movie.overview}</p> : null}
             </div>
             <div className='px-2 py-2 mb-0'>
                 <button type="button" className={this.state.like ? "m-1 mr-2 h-auto bg-blue-500 hover:bg-blue-400 text-white font-bold py-auto px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded outline-none" : "mr-2 h-auto bg-gray-500 hover:bg-gray-400 text-white font-bold py-auto px-4 border-b-4 border-gray-700 hover:border-gray-500 rounded focus:outline-none"} onClick={this.handleLike}>Like</button>
